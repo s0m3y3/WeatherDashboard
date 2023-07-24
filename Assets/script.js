d15a2e808e3dbd3ee8df1a2ebbd70cdbcfaa8713
Yee Y <s0m3y3@gmail.com>
2023-07-24 16:44:19 -0500

    Added jquery script. Added more elements to HTML. Updated Readme.

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,9 @@
-let APIKey = "40c69ea1d7b0101e5044f901e840aebc";
-let weatherAPI = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${APIKey}';
-let geocodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&appid=${APIkey}';
-
-const searchcity = document.getElementById('btn-primary'); 
-let cityname = document.getElementById("cityinput");
+// let APIKey = "40c69ea1d7b0101e5044f901e840aebc";
+let cityname ="";
+let lat = "";
+let lon = "";
+let weatherAPI = 'api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid=40c69ea1d7b0101e5044f901e840aebc';
+let geocodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?q='+cityname+'&appid=40c69ea1d7b0101e5044f901e840aebc';
 
 todayEl = document.getElementById("today");
 date1El = document.getElementById("date1");
@@ -20,19 +20,60 @@ Minneapolis = {	//weather api info for Minneapolis
   "country": "US",
   "state": "Minnesota"
 }
+//API GEO example. delete later. 
+apigeo = [
+	{
+		"name": "Minneapolis",
+		"local_names": {
+			"hi": "मिन्यापोलिस्",
+			"kn": "ಮಿನ್ಯಾಪೋಲಿಸ್",
+			"pt": "Mineápolis",
+			"fa": "مینیاپولیس",
+			"zh": "明尼阿波利斯",
+			"eo": "Mineapolo",
+			"en": "Minneapolis",
+			"ar": "منيابولس",
+			"ja": "ミネアポリス",
+			"ru": "Миннеаполис",
+			"uk": "Міннеаполіс",
+			"oc": "Minneapòlis",
+			"he": "מיניאפוליס",
+			"oj": "Gakaabikaang"
+		},
+		"lat": 44.9772995,
+		"lon": -93.2654692,
+		"country": "US",
+		"state": "Minnesota"
+	}
+];
 
-fetch(weatherAPI,)  //need work
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-});
+// fetch(weatherAPI,)  //need work
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+// });
 
-today = dayjs();
+// today = dayjs();
+// $('#todaydate').text(dayjs());
+// console.log(today);
+// console.log('hey');
+// let day1 = dayjs('2023-11-29');
+// $('#1a').text(day1.format('MMM D, YYYY'));
 
-
+let cityInput = document.getElementById("cityinput");
+  console.log(cityname);
+const searchcity = document.getElementById('btn-primary'); 
 
 searchcity.addEventListener("click",function(event){
-  //need work
-})
+  cityname = document.getElementById("cityinput");
+  console.log(cityname);
+});
+
+let setweatherforcast = function(){
+//code weekly grab
+//update search histoy.
+//get uv-index
+
+};
